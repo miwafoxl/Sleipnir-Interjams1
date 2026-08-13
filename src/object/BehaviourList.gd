28b1c34ffc627cab2f5d054a25302ac0320a3617
Amanda Severo <miwafoxl@proton.me>
2026-08-12 23:52:59 -0300

    behaviourlist: behaviour preprocessing method that automatically handles F_END, F_START and ALWAYS behaviour processing modes

--- src/object/BehaviourList.gd
+++ src/object/BehaviourList.gd
@@ -1,10 +1,18 @@
 class_name BehaviourList extends Node
 
+const LOG_VERBOSE: bool = true
+
 @export var enabled: bool = true
 @export var actors: Dictionary[String, Node] = {} # Actor ID: Node
 @export var behaviours: Array[Behaviour] = []
 
-var refill_actors: bool = true # Managed by BehaviourList
+# Managed by BehaviourList
+var refill_actors: bool = true 
+var vhigh_priority_pool: Array[Behaviour] = [] 
+var high_priority_pool: Array[Behaviour] = []
+var normal_priority_pool: Array[Behaviour] = []
+# End Managed by BehaviourList
+
 
 # Actor -> A node in which the behaviour needs to act upon
 # Behaviour -> An predictable operation where action is done to Actors
@@ -15,9 +23,11 @@ var refill_actors: bool = true # Managed by BehaviourList
  #region CONSOLE OUT
 
 static func _log_standard(message: String) -> void:
+	if not LOG_VERBOSE: return
 	print("[BehaviourList]: %s" % message)
 
 static func _log_warn(message: String) -> void:
+	if not LOG_VERBOSE: return
 	push_warning("[BehaviourList]: %s" % message)
 
 static func _log_err(message: String) -> void:
@@ -39,55 +49,89 @@ func get_behaviours(behaviour_name: StringName = &"") -> Array[Behaviour]:
 
 func remove_behaviours(behaviour_name: StringName = &"") -> void:
 	if name.is_empty(): return
-	var _indexes: Array[int]
+	var _count = behaviours.count(behaviour_name)
 	var _remove: Callable = func(_b: Behaviour) -> bool:
 		return not _b.name == behaviour_name
 	behaviours = behaviours.filter(_remove)
+	_log_standard("Removed %s '%s' behaviour(s)." % [_count, behaviour_name])
 
-func append_behaviour(behaviour: Behaviour, at_index: int = -1) -> bool:
+func insert_behaviour(behaviour: Behaviour, at_index: int = -1) -> bool:
+	preprocess_behaviours.call_deferred()
+	_log_standard("Inserted behaviour '%s'" % behaviour.name)
 	return OK == behaviours.insert(at_index, behaviour)
 	
 #endregion MANAGING BEHAVIOURS
 #region MANAGING ACTORS
 
+func get_actor(actor_name: String) -> Node:
+	return actors.get(actor_name, null)
 
+func remove_actor(actor_name: String) -> void:
+	if not actors.erase(actor_name):
+		_log_warn("Actor '%s' wasn't removed - Wasn't in actors" % actor_name)
+	_log_standard("Append actor '%s'" % actor_name)
+
+func append_actor(actor_name: String, node: Node) -> bool:
+	_log_standard("Append actor '%s'" % actor_name)
+	return actors.set(actor_name, node)
 
 #endregion MANAGING ACTORS
 #region BEHAVIOUR CLOCK
 
-# TODO: A behaviour with high priority and normal priority can be
-# ran at the same time since the high priority action will occur at
-# the start of the frame and the normal priority will occur at the
-# end of the frame. At the moment, normal priority ran at the end of
-# the frame but that has no difference from just running them directly,
-# since there's still one behaviour being checked per frame.
-#
-# The problem is that it will require me to separate HIGH and NORMAL
-# priority into two different ticks. First, I check the condition for
-# the NORMAL behaviour and ran, then I check if there's a HIGH priority
-# one to run alongside that.
-#
-# Maybe at _ready() I can pre-process behaviours array into other arrays
-# NormalPriority and HighPriority. The NormalPriority is checked first, 
-# runs deferred, then HighPriority is ran second and runs immediately.
-# VeryHighPriority can be checked before NormalPriority and ran immediately.
-
-var tick: int = 0
-func _process(_delta: float) -> void:
+func preprocess_behaviours() -> void:
+	var _auto: bool = false
+	var _high_priority_spool: Array[Behaviour] = [] # Subpool
+	var _normal_priority_spool: Array[Behaviour] = [] # Subpool
+	if behaviours.is_empty(): return
+	for _b: Behaviour in behaviours:
+		if _b.enabled:
+			match _b.process:
+				Behaviour.ProcessMode.F_END:
+					normal_priority_pool.append(_b)
+				Behaviour.ProcessMode.F_START:
+					high_priority_pool.append(_b)
+				Behaviour.ProcessMode.ALWAYS:
+					vhigh_priority_pool.append(_b)
+				_:
+					if _auto:
+						_normal_priority_spool.append(_b) 
+					else:
+						_high_priority_spool.append(_b)
+					_auto = not _auto
+	# Adds subpools to the end of master pool
+	for _b: Behaviour in _high_priority_spool:
+		high_priority_pool.append(_b)
+	for _b: Behaviour in _normal_priority_spool:
+		normal_priority_pool.append(_b)
+	_log_standard( \
+		"BehaviourList Preprocess:\n-F_END: %s\nF_START: %s\nALWAYS: %s" % \
+		[normal_priority_pool, high_priority_pool, vhigh_priority_pool])
+	# Reset ticks
+	tick_normal = 0
+	tick_high = 0
+	
+var tick_normal: int = 0
+var tick_high: int = 0
+func _process(delta: float) -> void:
 	if refill_actors:
 		for behaviour: Behaviour in behaviours:
 			behaviour.actors = actors
 		refill_actors = false
-	if enabled and tick < behaviours.size():
-		var _b: Behaviour = behaviours[tick]
-		if _b.enabled and _b.condition(_delta):
-			match _b.priority:
-				Behaviour.BehaviourPriority.HIGH:
-					_b.action(_delta)
-				Behaviour.BehaviourPriority.NORMAL:
-					_b.action.call_deferred(_delta)
-		tick += 1
-	else:
-		tick = 0
+	if not vhigh_priority_pool.is_empty():
+		for _b: Behaviour in vhigh_priority_pool:
+			if _b.condition(delta): _b.action(delta)
+	var _f_end_b: Behaviour = normal_priority_pool[tick_normal]
+	var _f_start_b: Behaviour = high_priority_pool[tick_high]
+	if _f_end_b.condition(delta): _f_end_b.action.call_deferred(delta)
+	if _f_start_b.condition(delta): _f_start_b.action(delta)
+	tick_normal = (tick_normal + 1) % (normal_priority_pool.size() - 1)
+	tick_high = (tick_high + 1) % (high_priority_pool.size() - 1)
 
-#endregion BEHAVIOUR CLOCKq
+#endregion BEHAVIOUR CLOCK
+
+#region OVERRIDES
+
+func _ready() -> void:
+	preprocess_behaviours()
+
+#endregion OVERRIDES
