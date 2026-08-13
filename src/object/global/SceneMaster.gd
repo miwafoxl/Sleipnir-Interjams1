8f68abd96a4a063a94c5285152cd30356ff53735
Amanda Severo <miwafoxl@proton.me>
2026-08-12 23:57:52 -0300

    scenemaster: move verbose checking to logger method

--- src/object/global/SceneMaster.gd
+++ src/object/global/SceneMaster.gd
@@ -11,9 +11,11 @@ static var loaded_scenes: Dictionary[String, PackedScene] = {}
 #region CONSOLE OUT
 
 static func _log_standard(message: String) -> void:
+	if not LOG_VERBOSE: return
 	print("[Scene]: %s" % message)
 
 static func _log_warn(message: String) -> void:
+	if not LOG_VERBOSE: return
 	push_warning("[Scene]: %s" % message)
 
 static func _log_err(message: String) -> void:
@@ -29,23 +31,23 @@ static func swap_scene(loaded_scene: String) -> bool:
 	var _scene: PackedScene = loaded_scenes.get(loaded_scene, null)
 	var _new: Node = null
 	if not loaded_scenes.has(loaded_scene) or _scene == null:
-		if LOG_VERBOSE: _log_warn("Failed to swap to scene '%s' - No such scene loaded" % loaded_scene)
+		_log_warn("Failed to swap to scene '%s' - No such scene loaded" % loaded_scene)
 		return false
 	unload_current()
 	_new = _scene.instantiate()
 	current_scene = weakref(_new)
 	var _main: Node = SceneTree.root
 	_main.add_child.call_deferred(_new)
-	if LOG_VERBOSE: _log_standard("Swapping to scene '%s'" % loaded_scene)
+	_log_standard("Swapping to scene '%s'" % loaded_scene)
 	return true
 	
 static func load_scene_path(scene_name: String, path: String, swap: bool = false) -> bool:
 	var _scn: PackedScene = load(path)
 	if _scn == null:
-		if LOG_VERBOSE: _log_warn("Failed to load scene '%s'" % scene_name)
+		_log_warn("Failed to load scene '%s'" % scene_name)
 		return false
 	if loaded_scenes.has(scene_name):
-		if LOG_VERBOSE: _log_standard("Overwriting already loaded scene '%s'" % scene_name)
+		_log_standard("Overwriting already loaded scene '%s'" % scene_name)
 	loaded_scenes.set(scene_name, _scn)
 	if swap: swap_scene(scene_name)
 	return true
@@ -53,19 +55,19 @@ static func load_scene_path(scene_name: String, path: String, swap: bool = false
 static func load_scenes(scenes: Dictionary[String, PackedScene]) -> void:
 	for scene_name: String in scenes.keys():
 		if loaded_scenes.has(scene_name):
-			if LOG_VERBOSE: _log_standard("Overwriting already loaded scene '%s'" % scene_name)
+			_log_standard("Overwriting already loaded scene '%s'" % scene_name)
 		loaded_scenes.set(scene_name, scenes[scene_name])
 
 static func unload_current() -> void:
 	var _cur: Node = get_current()
 	if not _cur == null: 
 		_cur.queue_free()
-		if LOG_VERBOSE: _log_standard("Unloading current scene")
+		_log_standard("Unloading current scene")
 
 static func unload_scene(scene_name: String) -> void:
 	if loaded_scenes.has(scene_name):
 		loaded_scenes.erase(scene_name)
-		if LOG_VERBOSE: _log_standard("Unloaded scene '%s'" % scene_name)
+		_log_standard("Unloaded scene '%s'" % scene_name)
 
 #endregion SCENE MANAGING
 #region OVERRIDES
