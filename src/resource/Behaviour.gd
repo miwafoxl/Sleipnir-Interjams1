1767ea0a4ba370459fd822eb0909dd5216880500
Amanda Severo <miwafoxl@proton.me>
2026-08-12 23:53:56 -0300

    behaviour: renamed BehaviourPriority to ProcessMode, added documentation comments

--- src/resource/Behaviour.gd
+++ src/resource/Behaviour.gd
@@ -1,34 +1,49 @@
+@abstract
 class_name Behaviour extends Resource
 
-@export var name: StringName = &""
-@export var enabled: bool = true
-@export var priority: BehaviourPriority = BehaviourPriority.NORMAL
+@export var name: StringName = &"" ## A StringName that will be used for future referencing.
+@export var enabled: bool = true ## Sets if the behaviour is currently active
+@export var process: ProcessMode = ProcessMode.AUTOMATIC ## The behaviour's mode of operation. See [b]ProcessMode[/b] for more.
 
-enum BehaviourPriority {
-	NORMAL,		# Action is ran at the end of the frame
-	HIGH,		# Action is ran at the start of the frame
-	VERY_HIGH,	# TODO: Condition is checked frame by frame,
-				# Action is ran at the start of the frame
+## Controls the order in which this behaviour is checked and ran.[br][br]
+## - [code]AUTOMATIC[/code]: Alternates between [code]F_START[/code]
+## and [code]F_END[/code].[br][br]
+## - [code]F_START[/code]: Runs [code]action()[/code] immediately
+## after [code]condition()[/code] returns true.[br][br]
+## - [code]F_END[/code]: Runs [code]action()[/code] deferred after 
+## [code]condition()[/code] returns true.[br][br]
+## - [code]ALWAYS[/code]: Checks and runs [code]action()[/code] for
+## each frame before everything else.[br][br]
+## [b]Warning:[/b] [code]ALWAYS[/code] should be sparingly as it might
+## slow down runtime performance.
+enum ProcessMode {
+	AUTOMATIC, ## Alternates between [code]F_START[/code] and [code]F_END[/code].
+	F_START, ## Runs [code]action()[/code] immediately after [code]condition()[/code] returns true.
+	F_END, ## Runs [code]action()[/code] deferred after [code]condition()[/code] returns true.
+	ALWAYS, ## Checks and runs [code]action()[/code] for each frame before everything else.
 }
 
-# This variable will be autofilled by the BehaviourList before
-# condition() and action() is ran.
+## The [code]BehaviourList[/code] will populate this field with context nodes in which 
+## [code]action()[/code] and [code]condition()[/code] can borrow values from.
 var actors: Dictionary[String, Node]
 
 #region MANAGING BEHAVIOUR
 
+## Sets this Behaviour enabled. 
 func set_enabled(enable: bool) -> void:
 	enabled = enable
 
 #endregion MANAGING BEHAVIOUR
 #region OVERRIDEABLES
 
-@warning_ignore("unused_parameter")
-func condition(delta: float) -> bool:
-	return true
+## This method is called automatically by [code]BehaviourList[/code] where it will test
+## whether if [code]action()[/code] can be ran.
+@abstract
+func condition(delta: float) -> bool
 
-@warning_ignore("unused_parameter")
-func action(delta: float) -> void:
-	return
+## This method is called automatically by [code]BehaviourList[/code], which provides
+## action for a met criteria given by [code]condition()[/code].
+@abstract
+func action(delta: float) -> bool
 
 #endregion OVERRIDEABLES
