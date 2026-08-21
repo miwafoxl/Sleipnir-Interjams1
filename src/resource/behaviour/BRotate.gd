cc7e3763c0bda8089e7eec51c30c5404686258cf
Amanda Severo <miwafoxl@proton.me>
2026-08-21 15:12:20 -0300

    behaviours: edit BRotate to use a specific actor to rotate instead of self

--- src/resource/behaviour/BRotate.gd
+++ src/resource/behaviour/BRotate.gd
@@ -2,13 +2,17 @@ class_name BRotate extends Behaviour
 
 @export var amount: float = 0.05
 
-var player: Sprite2D
+var rotating: Sprite2D
 
 func init() -> void:
-	player = get_actor() as Sprite2D
+	var _rotator: Sprite2D = get_actor("rotate")
+	if _rotator == null:
+		rotating = get_actor() as Sprite2D
+		return
+	rotating = _rotator
 
 func condition(_delta: float) -> bool:
 	return true
 
 func action(_delta: float) -> void:
-	player.rotate(-amount)
+	rotating.rotate(-amount)
--- src/scene/testing.gd
+++ src/scene/testing.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
-@onready var test: BehaviourList = BehaviourList.get_behaviourlist($Icon3)
+@onready var test: GameObject = GameObject.get_gameobject($Icon3)
 
 func _ready() -> void:
 	print_debug(test)
 
 func _on_icon_but_pressed() -> void:
-	test.add_actor("player", $Icon)
+	test.add_actor("rotate", $Icon)
 
 func _on_icon_2_but_pressed() -> void:
-	test.add_actor("player", $Icon2)
+	test.add_actor("rotate", $Icon2)
 
 func _on_icon_3_but_pressed() -> void:
-	test.add_actor("player", $Icon3)
+	test.add_actor("rotate", $Icon3)
